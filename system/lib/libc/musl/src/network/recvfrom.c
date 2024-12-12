fbaa74d80c64fcf34a72bb46af6f4138b841cb3b
Angel M Miguel <hi@angel.kiwi>
2024-12-12 10:55:27 +0100

    feat: introduce testing sleep in recvfrom.c musl

--- system/lib/libc/musl/src/network/recvfrom.c
+++ system/lib/libc/musl/src/network/recvfrom.c
@@ -1,7 +1,9 @@
 #include <sys/socket.h>
 #include "syscall.h"
+#include "emscripten.h"
 
 ssize_t recvfrom(int fd, void *restrict buf, size_t len, int flags, struct sockaddr *restrict addr, socklen_t *restrict alen)
 {
+	emscripten_sleep(1000);
 	return socketcall_cp(recvfrom, fd, buf, len, flags, addr, alen);
 }
